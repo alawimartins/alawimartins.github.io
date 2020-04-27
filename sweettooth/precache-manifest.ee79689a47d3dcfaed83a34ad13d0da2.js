44ebbbe5ec7f93feefce6ecb5ba7a45048e8c69c
alawimartins <yasminmartins.gt@gmail.com>
2020-04-27 10:24:09 +0100

    last commit

---
+++ sweettooth/precache-manifest.ee79689a47d3dcfaed83a34ad13d0da2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4b7444acfd05f5b4b53116cd1fdc650",
+    "url": "/sweettooth/index.html"
+  },
+  {
+    "revision": "7a1d9b6bcb900c75d40b",
+    "url": "/sweettooth/static/css/main.bd7178db.chunk.css"
+  },
+  {
+    "revision": "e3715e84dbbac1a980c8",
+    "url": "/sweettooth/static/js/2.a907ebb5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sweettooth/static/js/2.a907ebb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a1d9b6bcb900c75d40b",
+    "url": "/sweettooth/static/js/main.e5e3fc2c.chunk.js"
+  },
+  {
+    "revision": "9943601e07676e04e6ad",
+    "url": "/sweettooth/static/js/runtime-main.3c80d1a1.js"
+  },
+  {
+    "revision": "646f826f03b1bf9c5fdb58d60312d38b",
+    "url": "/sweettooth/static/media/background.646f826f.png"
+  }
+]);
